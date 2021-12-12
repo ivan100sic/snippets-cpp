fdb57b5adfdefca635522efa361335b45e6f7d52
Ivan Stosic <ivan100sic@gmail.com>
2021-12-12 15:11:51 +0100

    Add script to generate snippets json

---
+++ bash/gen-vscode.sh
@@ -0,0 +1,3 @@
+read -p "C++ standard: " STD
+ROOT=`git rev-parse --show-toplevel`
+$ROOT/build/generate `find $ROOT/snippets/ -name "*.cpp"` --cpp-standard=$STD > $ROOT/vscode-cpp$STD.code-snippets
