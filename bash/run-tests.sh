8f3314c59878b4b12d3c80d825472ac07997c855
Ivan Stosic <ivan100sic@gmail.com>
2021-12-30 22:14:42 +0100

    Added bash script to run tests on snippets

---
+++ bash/run-tests.sh
@@ -0,0 +1,4 @@
+read -p "Threads: " THR
+read -p "Compiler path: " CXX
+ROOT=`git rev-parse --show-toplevel`
+$ROOT/build/test --threads=$THR --compiler-path=$CXX `find $ROOT/snippets/ -name "*.cpp"`
